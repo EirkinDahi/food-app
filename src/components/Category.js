1c5ebc37112f7c89b747efdd90cf533603a04051
Eirkin Dahi <eirkin12@gmail.com>
2023-01-25 01:15:41 -0500

    add routing to the different cuisines

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,27 +2,27 @@ import {FaPizzaSlice, FaHamburger} from 'react-icons/fa';
 import {GiNoodles,GiChopsticks} from 'react-icons/gi';
 import styled from 'styled-components';
 import {} from 'react-router-dom';
-// import { div } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Category = () => {
   return (
     <List>
-        <div>
+        <NavLink to = {'/cuisine/italian'}>
             <FaPizzaSlice/>
             <h4>Italian</h4>
-        </div>
-        <div>
+        </NavLink>
+        <NavLink to={'/cuisine/american'}>
             <FaHamburger/>
             <h4>American</h4>
-        </div>
-        <div>
+        </NavLink>
+        <NavLink to = {'/cuisine/thai'}>
             <GiNoodles/>
             <h4>Thai</h4>
-        </div>
-        <div>
+        </NavLink>
+        <NavLink to = {'/cuisine/japanese'}>
             <GiChopsticks/>
             <h4>Japanese</h4>
-        </div>
+        </NavLink>
     </List>
   )
 }
